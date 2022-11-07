1b5ef1ea8435a3a1fc53d93b117c53b0196b9aec
George Wortmann <gf.wortmann@gmail.com>
2022-11-07 21:09:59 +0300

    Task 26 committed 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задача 26 выдает количество цифр в числе
+//
+int GetRange(int a)
+{
+    int i = 0;
+    while(a>1)
+    {
+        a /= 10;
+        i++;
+        Console.WriteLine($"{a}: {i}");
+    }
+    return i;
+}
+
+Console.Clear();
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetRange(A));
